5678146b818906a8689743a25bf80c0ec6ee7530
nathanmargaglio <nathanmargaglio@gmail.com>
2017-05-29 16:07:35 -0400

    Fixed Income model

--- rest_api/models.py
+++ rest_api/models.py
@@ -8,12 +8,13 @@ class Income(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     school = models.CharField(max_length=128, default=None, null=True)
-    date = models.DateTimeField(default=None, null=True)
-    check = models.CharField(max_length=128, default=None, null=True)
-    cash = models.CharField(max_length=128, default=None, null=True)
-    credit_card = models.CharField(max_length=128, default=None, null=True)
-    vending = models.CharField(max_length=128, default=None, null=True)
-    ez_payment = models.CharField(max_length=128, default=None, null=True)
+    date = models.DateField(default=None, null=True)
+
+    check = models.FloatField(max_length=128, default=None, null=True)
+    cash = models.FloatField(max_length=128, default=None, null=True)
+    credit_card = models.FloatField(max_length=128, default=None, null=True)
+    vending = models.FloatField(max_length=128, default=None, null=True)
+    ez_payment = models.FloatField(max_length=128, default=None, null=True)
 
     def __str__(self):
         return "Created: " + str(self.created)
